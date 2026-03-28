4073edfcf4362576b64c94e5224c84f7a003e84f
Nizamuddin45 <udnizam45@gmail.com>
2026-03-28 18:12:30 +0530

    remove exception file

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+# import logging
 
 def error_message_detail(error,error_detail:sys): # jo bhi error aaye mai usse apne own custom message me push krna hai mujhe 
     _,_,exc_tb = error_detail.exc_info() # basically gives 3 info pehle 2 se kaam nhi hai 3rd is important .tb waala ye btata hai kis file me kis line me rror hai
@@ -16,9 +16,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__== '__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)    
+# if __name__== '__main__':
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)    
