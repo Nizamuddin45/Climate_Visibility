c046ec738ff08104f5036396d585fb51d1d3b481
Nizamuddin45 <udnizam45@gmail.com>
2026-04-16 11:28:44 +0530

    Updated trained model

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,107 +12,112 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
-from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config=ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
-
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
-            logging.info("Split training and test input data")
-            X_train,y_train,X_test,y_test=(
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+            logging.info("Splitting training and test data")
+
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1],
             )
+
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False),
+                "AdaBoost": AdaBoostRegressor(),
             }
-            params={
+
+            params = {
                 "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error']
                 },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Random Forest": {
+                    'n_estimators': [50, 100, 200]
                 },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Gradient Boosting": {
+                    'learning_rate': [0.1, 0.05],
+                    'n_estimators': [50, 100],
+                    'subsample': [0.8, 1.0]
                 },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.05],
+                    'n_estimators': [50, 100]
                 },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
+                "CatBoost": {
+                    'depth': [6, 8],
+                    'learning_rate': [0.01, 0.05],
+                    'iterations': [50, 100]
                 },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
+                "AdaBoost": {
+                    'learning_rate': [0.1, 0.05],
+                    'n_estimators': [50, 100]
                 }
-                
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+            best_model = None
+            best_score = -1
+            best_model_name = ""
+
+            #Training + Hyperparameter Tuning
+            for model_name, model in models.items():
+                logging.info(f"Training {model_name}")
+
+                param_grid = params[model_name]
+
+                if param_grid:
+                    grid = GridSearchCV(model, param_grid, cv=3, scoring='r2', n_jobs=-1,verbose=2)
+                    grid.fit(X_train, y_train)
+                    trained_model = grid.best_estimator_
+                else:
+                    model.fit(X_train, y_train)
+                    trained_model = model
+
+                # Evaluate
+                y_pred = trained_model.predict(X_test)
+                score = r2_score(y_test, y_pred)
+
+                logging.info(f"{model_name} R2 Score: {score}")
+
+                if score > best_score:
+                    best_score = score
+                    best_model = trained_model
+                    best_model_name = model_name
+
+            if best_score < 0.6:
+                raise CustomException("No good model found", sys)
+
+            logging.info(f"Best Model: {best_model_name} with score {best_score}")
+
             
-            ## To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
-
-            ## To get best model name from dict
-
-            best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
-            ]
-            best_model = models[best_model_name]
-
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
-
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
 
-            predicted=best_model.predict(X_test)
+            return best_score
 
-            r2_square = r2_score(y_test, predicted)
-            return r2_square
-            
-
-
-
-            
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,58 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline():
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled  = preprocessor.transform(features) 
+            preds = model.predict(data_scaled)
+            return preds
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+class CustomData():
+    def __init__(self,
+            DRYBULBTEMPF: float ,
+            RelativeHumidity: float, WindSpeed: float, WindDirection: float,
+            SeaLevelPressure: float, year: int, month: int, day: int, hour: int):
+        
+            self.Dry_Bulb_Tempf = DRYBULBTEMPF
+            self.Relative_Humidity = RelativeHumidity
+
+            self.Wind_Speed = WindSpeed
+            self.Wind_Direction = WindDirection
+            self.SeaLevel_Pressure =  SeaLevelPressure
+            self.year = year
+            self.month = month
+            self.day = day
+            self.hour = hour
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "DRYBULBTEMPF": [self.Dry_Bulb_Tempf],
+                "RelativeHumidity" : [self.Relative_Humidity],
+                "WindSpeed": [self.Wind_Speed],
+                "WindDirection": [self.Wind_Direction],
+                "SeaLevelPressure": [self.SeaLevel_Pressure],
+                "year": [self.year],
+                "month": [self.month],
+                "day": [self.day],
+                "hour": [self.hour]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+             raise CustomException(e,sys)
+        
+
